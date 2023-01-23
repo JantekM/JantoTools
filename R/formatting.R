f2a0507e1f4a74dcf75111bf1b6a7aade4e3c0f6
Jantek Mikulski <jantoni.mikulski@gmail.com>
2023-01-23 14:11:49 +0100

    signif_sym(...) tests and fixes

--- R/formatting.R
+++ R/formatting.R
@@ -1,10 +1,12 @@
-#' Changes p-value number to a significance symbol for decorative purposes
+#' Changes p-value number to a significance symbol for decorative purposes.
+#'
+#' Any NAs in the input are always transformed to "". Probabilities outside range <0,1> raise an error.
 #'
 #' @param x A numeric vector of input probabilities
 #' @param max_asterisks Number of asterisks (*) for the most significant results, default is 5. Cannot be smaller than 2. Set to 3 for APA/NEJM standards.
 #' @param bonferoni Number of tests in a family of tests when applying the Bonferroni correction for multiple comparisons problem. Is 1 by default which is same as not applying any correction.
 #' Use it only if the passed p-values are not corrected already.
-#' @param NA_instead_of_error Logical, by default (FALSE) if the functions sees any probability outside <0,1> it raises an error. However this behaviour can be changed to return NAs instead.
+## @param NA_instead_of_error Logical, by default (FALSE) if the functions sees any probability outside <0,1> it raises an error. However this behaviour can be changed to return NAs instead.
 #' @param include_dot Logical, default TRUE, should the dot symbol be used for borderline significance that is between 5% and 10%?
 #' @param ns_sym Character (default ' '), the symbol used for the non-significant results. Set to 'ns' APA/NEJM standards.
 #'
@@ -14,19 +16,29 @@
 #' @examples
 #' x <- 3e-4
 #' signif_sym(x)
-signif_sym <- function(x, max_asterisks=5, bonferoni = 1, NA_instead_of_error = F, include_dot = T, ns_sym = ' ') {
-  if(!NA_instead_of_error){
-    if(any(x<0) | any(x>1)){
-      stop("Error in signif_sym(...), passed probabilities cannot be smaller than 0 or bigger than 1 if NA_instead_of_error param is set to FALSE.")
-    }
+signif_sym <-
+  function(x,
+           max_asterisks = 5,
+           bonferoni = 1,
+           #NA_instead_of_error = F,
+           include_dot = T,
+           ns_sym = ' ') {
+
+  #if(!NA_instead_of_error){
+  if(any(stats::na.omit(x) < 0) | any(stats::na.omit(x)>1)){
+    stop("Error in signif_sym(...), passed probabilities cannot be smaller than 0 or bigger than 1.")
   }
+  #}
   if(max_asterisks<2){
     stop("Error in signif_sym(...), max_asterisks param has to be at least 2.")
   }
+  if(is.na(ns_sym)){
+    stop("Error in signif_sym(...), ns_sym cannot be set to NA.")
+  }
   cutpoints = rep(0.1, max_asterisks-1) ^ seq(from = max_asterisks, to = 2)
-  cutpoints = c(0, cutpoints, 0.05, ifelse(include_dot, 0.1, NULL), 1)
+  cutpoints = c(0, cutpoints, 0.05, ifelse(include_dot, 0.1, NA), 1) %>% stats::na.omit()
   symbols = sapply(max_asterisks:1, function(x){paste(rep('*', x), collapse='')})
-  symbols = c(symbols,ifelse(include_dot, '.', NULL), ns_sym)
+  symbols = c(symbols,ifelse(include_dot, '.', NA), ns_sym) %>% stats::na.omit()
 
   # output for default 5 asterisks with dot and space as ns_sym
   # cutpoints = c(0,       0.00001, 0.0001, 0.001, 0.01, 0.05, 0.1, 1)
--- tests/testthat/test-formatting.R
+++ tests/testthat/test-formatting.R
@@ -1,3 +1,20 @@
 test_that("signif_sym() basic work", {
   expect_equal(signif_sym(3e-4), "***")
+  expect_equal(signif_sym(c(0.05, 0, 1, 0.1)), c("*", "*****", " ", "."))
+  expect_error(signif_sym(-1))
+  expect_error(signif_sym(2))
+  expect_equal(signif_sym(c(1, 0.5, NA, 0.03, 0.008, 0.0008, NA)), c(" "," ","","*","**","***",""))
+})
+test_that("signif_sym() custom params", {
+  expect_equal(signif_sym(3e-10, max_asterisks = 5), "*****")
+  expect_equal(signif_sym(3e-10, max_asterisks = 6), "******")
+  expect_equal(signif_sym(3e-10, max_asterisks = 2), "**")
+  expect_error(signif_sym(3e-10, max_asterisks = 1))
+
+  expect_equal(signif_sym(c(1, 0.5, 0.07, 0.03, 0.008, 0.0008, 0.00008), bonferoni = 1), c(" "," ",".","*","**","***","****"))
+  expect_equal(signif_sym(c(1, 0.5, 0.07, 0.03, 0.008, 0.0008, 0.00008), bonferoni = 4), c(" "," ","*","**","**","***","****"))
+  expect_equal(signif_sym(c(1, 0.5, 0.07, 0.03, 0.008, 0.0008, 0.00008), bonferoni = 14), c(".","*","**","**","***","****","*****"))
+
+  expect_equal(signif_sym(c(1, 0.5, 0.07, 0.03, 0.008, 0.0008, 0.00008), include_dot = F), c(" "," "," ","*","**","***","****"))
+  expect_equal(signif_sym(c(1, 0.5, 0.07, 0.03, 0.008, 0.0008, 0.00008), include_dot = F, ns_sym = 'ns'), c("ns","ns","ns","*","**","***","****"))
 })
